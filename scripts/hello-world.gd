fae03e7dafd8ce3c82ba3a9a099c99e10c0b8eb1
Vinícius <viniciushallmann2015@gmail.com>
2024-01-08 16:48:14 -0300

    commit att

--- scripts/hello-world.gd
+++ scripts/hello-world.gd
@@ -3,13 +3,11 @@ extends Node
 func _ready():
 	var nameVini = "Vini"
 	print("Hello " + nameVini)
-
+	
 	var nameJoao = "Joao"
 	print("Hello "+ nameJoao)
-<<<<<<< HEAD
-=======
-
+	
 	var nameHenrique = "Henrique"
 	print("Hello " + nameHenrique)
 	
->>>>>>> 4638a81b07e4a86c0acac5ffd24cbe95a00fc830
+
